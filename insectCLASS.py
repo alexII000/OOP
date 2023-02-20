16648e4457d1f261eafda81774ae1cfe1b4ef587
AlexII000 <alex_chon1@baylor.edu>
2023-02-20 14:56:23 -0600

    s

--- insectCLASS.py
+++ insectCLASS.py
@@ -8,11 +8,15 @@ class Insect:
         self.wing = w
         self.legs = l
         self.flight = 0
+        self.mile = 0
 
-    def flight_length(self):
-        self.flight = random.randint(1,10)
+    def fly(self):
+        self.mile = random.randint(1,10)
 
-    def get_miles(self):
-        return self.flight
+    def get_fly(self):
+        return self.mile
+
+    def get_name(self):
+        return self.name
         # return self.wings
         # return self.legs
--- insect_generator.py
+++ insect_generator.py
@@ -4,16 +4,16 @@ import insectCLASS as i
 mosquito = i.Insect('mosquito',2,4)
 housefly = i.Insect('housefly',2,4)
 
-mosquito.flight_length()
-housefly.flight_length()
+mosquito.get_fly()
+housefly.get_fly()
 
 
 def main():
 
     my_insect = i.Insect()
 
-    print(f'The {mosquito.get_name()} flies for:', {mosquito.get_miles()})
-    print(f'The {housefly.get_name()} can fly up to {housefly.getmiles()})
+    print(f'The {mosquito.get_name()} flies for: {mosquito.get_fly()}')
+    print(f'The {housefly.get_name()} can fly up to {housefly.get_fly()}')
 
 
 main()
